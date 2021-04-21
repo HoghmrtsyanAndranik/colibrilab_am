7af323b3b6a1d64e219571f2876134c6d836afa0
Albart9800 <hoghmrtsyan.and@yandex.ru>
2021-04-21 17:40:41 +0400

    Azi changes

--- questions.php
+++ questions.php
@@ -102,7 +102,7 @@ elseif($LANG=='ENG'){
     <div class="acc-head card p-3 rounded-0 accord-bg accord-bg">
      Do I need prior knowledge to participate in classes?
     </div>
-    <div class="acc-body rounded-0 accord-bg1">
+    <div class="acc-body rounded-0 accord-bg1" style="padding-left: 11px; padding: 12px; font-size: 15px; color:#272727; background-color: white">
     No prior knowledge is required to participate in the courses, but a little knowledge of English is desirable.
     </div>
   </div>
@@ -110,7 +110,7 @@ elseif($LANG=='ENG'){
     <div class="acc-head card p-3 rounded-0 accord-bg">
       Who are the courses for?
     </div>
-    <div class="acc-body rounded-0 accord-bg1">
+    <div class="acc-body rounded-0 accord-bg1" style="padding-left: 11px; padding: 12px; font-size: 15px; color:#272727; background-color: white">
      The courses are designed for both beginners and those who have some basic knowledge.
     </div>
   </div>
@@ -118,7 +118,7 @@ elseif($LANG=='ENG'){
     <div class="acc-head card p-3 rounded-0 accord-bg">
       Do you provide us with computers?
     </div>
-    <div class="acc-body rounded-0 accord-bg1">
+    <div class="acc-body rounded-0 accord-bg1" style="padding-left: 11px; padding: 12px; font-size: 15px; color:#272727; background-color: white">
        Our classrooms are technically equipped. However, if you wish, you can participate in the course with your laptop.
     </div>
   </div>
@@ -126,7 +126,7 @@ elseif($LANG=='ENG'){
     <div class="acc-head card p-3 rounded-0 accord-bg">
       Do you provide a certificate at the end of the course?
     </div>
-    <div class="acc-body rounded-0 accord-bg1">
+    <div class="acc-body rounded-0 accord-bg1" style="padding-left: 11px; padding: 12px; font-size: 15px; color:#272727; background-color: white">
       After successfully completing the course you are given a certificate.
     </div>
   </div>
@@ -134,7 +134,7 @@ elseif($LANG=='ENG'){
     <div class="acc-head card p-3 rounded-0 accord-bg">
       Is it possible to get a job after graduating the course?
     </div>
-    <div class="acc-body rounded-0 accord-bg1">
+    <div class="acc-body rounded-0 accord-bg1" style="padding-left: 11px; padding: 12px; font-size: 15px; color:#272727; background-color: white">
       After completing the course, the students with high marks are given the opportunity of an internship, which, if successful, gives an opportunity to work in our or our partner organizations.
     </div>
   </div>
@@ -142,7 +142,7 @@ elseif($LANG=='ENG'){
     <div class="acc-head card p-3 rounded-0 accord-bg">
        What kind of classes do you organize: individual or group trainings?
     </div>
-    <div class="acc-body rounded-0 accord-bg1">
+    <div class="acc-body rounded-0 accord-bg1" style="padding-left: 11px; padding: 12px; font-size: 15px; color:#272727; background-color: white">
        Our classes are held in both group and individual versions.
     </div>
   </div>
@@ -150,9 +150,10 @@ elseif($LANG=='ENG'){
     <div class="acc-head card p-3 rounded-0 accord-bg">
       How can I enroll in programming classes?
     </div>
-    <div class="acc-body rounded-0 accord-bg1">
+    <div class="acc-body rounded-0 accord-bg1" style="padding-left: 11px; padding: 12px; font-size: 15px; color:#272727; background-color: white">
      You can register for the courses both by calling and by registering through the website.
   </div>
+</div>
 </div>
 
 
@@ -165,6 +166,7 @@ elseif($LANG=='ENG'){
 
 
   <script type="text/javascript">
+    $(".acc-body").slideUp();
       $(document).ready(function(){
   $('.acc-head').click(function(){
     $(this).next().slideToggle(500);
